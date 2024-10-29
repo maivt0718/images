3e6f4da74bac247c8ee8f7765b25e7c9eb9b73f2
Hana <maivt0718@gmail.com>
2024-10-29 16:10:17 +0700

    done: images

--- src/controllers/images.controller.js
+++ src/controllers/images.controller.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { status } from "../../utils/const.js";
 
 const prisma = new PrismaClient({
-    log: ["query"]
+  log: ["query"],
 });
 
 export const imagesController = {
@@ -16,25 +16,145 @@ export const imagesController = {
   },
   findImageByName: async (req, res, next) => {
     try {
-        let { name } = req.query
-        let searchString = `${name}`
-        let images = await prisma.hinh_anh.findMany({
-            where:{
-                ten_hinh:{
-                    contains: `${searchString}`
-                }
-            }
-        })
-        // let images = await prisma.$queryRaw`SELECT * FROM hinh_anh WHERE ten_hinh LIKE ${searchString}`
-        if(images.length === 0){
-            return res.status(status.NOT_FOUND).json({message: "Name is not found"})
-        }
-        return res.status(status.OK).json(images)
+      let { name } = req.query;
+      let searchString = `${name}`;
+      let images = await prisma.hinh_anh.findMany({
+        where: {
+          ten_hinh: {
+            contains: `${searchString}`,
+          },
+        },
+      });
+      // let images = await prisma.$queryRaw`SELECT * FROM hinh_anh WHERE ten_hinh LIKE ${searchString}`
+      if (images.length === 0) {
+        return res
+          .status(status.NOT_FOUND)
+          .json({ message: "Name is not found" });
+      }
+      return res.status(status.OK).json(images);
     } catch (error) {
-        return res.status(status.INTERNAL_SERVER).json({ message: `${error}` });
+      return res.status(status.INTERNAL_SERVER).json({ message: `${error}` });
     }
   },
-  getImageInfoAndItsOwner: (req, res, next) => {
-    
-  }
+  getImageInfoAndItsOwner: async (req, res, next) => {
+    try {
+      let { id } = req.body;
+      let image = await prisma.hinh_anh.findMany({
+        where: {
+          hinh_id: id,
+        },
+        select: {
+          hinh_id: true,
+          ten_hinh: true,
+          mo_ta: true,
+          nguoi_dung: {
+            select: {
+              nguoi_dung_id: true,
+              email: true,
+              ho_ten: true,
+              tuoi: true,
+              anh_dai_dien: true,
+            },
+          },
+        },
+      });
+      return res.status(status.OK).json(image);
+    } catch (error) {
+      return res.status(status.INTERNAL_SERVER).json({ message: `${error}` });
+    }
+  },
+  getCommentsByImageID: async (req, res, next) => {
+    try {
+      let { imageID } = req.params;
+      let image = await prisma.hinh_anh.findMany({
+        where: {
+          hinh_id: Number(imageID),
+        },
+        select: {
+          hinh_id: true,
+          ten_hinh: true,
+          mo_ta: true,
+          binh_luan: {
+            select: {
+              binh_luan_id: true,
+              ngay_binh_luan: true,
+              noi_dung: true,
+            },
+          },
+        },
+      });
+      return res.status(status.OK).json(image);
+    } catch (error) {
+      return res.status(status.INTERNAL_SERVER).json({ message: `${error}` });
+    }
+  },
+  checkSaveImage: async (req, res, next) => {
+    try {
+      let { imageID } = req.params;
+      let isNotSave = await prisma.hinh_anh.findMany({
+        where: {
+          hinh_id: Number(imageID),
+          luu_anh: {
+            none: {},
+          },
+        },
+      });
+      if (isNotSave.length) {
+        return res
+          .status(status.NOT_FOUND)
+          .json({ message: `Image is not saved`, data: isNotSave });
+      }
+      return res
+        .status(status.OK)
+        .json({ message: `Image is saved`, data: isNotSave });
+    } catch (error) {
+      return res.status(status.INTERNAL_SERVER).json({ message: `${error}` });
+    }
+  },
+  saveCommentWithImage: async (req, res, next) => {
+    try {
+      let { user_id, image_id, comment } = req.body;
+      let isCheckUserIDAndImageIDExisted = await prisma.binh_luan.findMany({
+        where: {
+          AND: {
+            nguoi_dung_id: user_id,
+            hinh_id: image_id,
+          },
+        },
+      });
+
+      let saveComment;
+      if (isCheckUserIDAndImageIDExisted.length == 0) {
+        saveComment = await prisma.binh_luan.create({
+          data: {
+            hinh_anh: { connect: { hinh_id: image_id } },
+            noi_dung: comment,
+            ngay_binh_luan: new Date().toISOString(),
+            nguoi_dung: {
+              connect: {
+                nguoi_dung_id: user_id,
+              },
+            },
+          },
+        });
+      } else {
+        saveComment = await prisma.binh_luan.update({
+          data: {
+            noi_dung: comment,
+          },
+          where: {
+            nguoi_dung_id_hinh_id: {
+              nguoi_dung_id: user_id,
+              hinh_id: image_id,
+            },
+          },
+        });
+      }
+      return res
+        .status(status.OK)
+        .json({ message: `Update successfully`, data: saveComment });
+    } catch (error) {
+      return res.status(status.INTERNAL_SERVER).json({ message: `${error}` });
+    }
+  },
 };
--- src/routes/images.router.js
+++ src/routes/images.router.js
@@ -5,6 +5,9 @@ const imagesRouter = express.Router()
 
 imagesRouter.get('/getAllImages', imagesController.getAllImages)
 imagesRouter.get('/getImagesByName', imagesController.findImageByName)
-
+imagesRouter.get('/getImagesAndItsOwner', imagesController.getImageInfoAndItsOwner)
+imagesRouter.get('/getCommentsByImageID/:imageID', imagesController.getCommentsByImageID)
+imagesRouter.get('/checkSaveImage/:imageID', imagesController.checkSaveImage)
+imagesRouter.post('/saveCommentWithImage', imagesController.saveCommentWithImage)
 
 export default imagesRouter
